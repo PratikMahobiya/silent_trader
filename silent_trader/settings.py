0505e8918be03e8365e02fcf4cb9d332a1b32828
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-04 16:56:41 +0530

    DATABASE_DETAILS_UPDATED

--- silent_trader/settings.py
+++ silent_trader/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'silent_trader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'hap_trading',
-#         'USER': 'hap',
-#         'PASSWORD': 'HAPbrothers2015',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hap_trading',
+        'USER': 'hap',
+        'PASSWORD': 'HAPbrothers2015',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
