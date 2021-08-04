fb8321f2d96f172ff3647c87a60b4f018893493c
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-04 18:07:44 +0530

    ADMIN_UPDATE

--- algo/admin.py
+++ algo/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.Transactions)
-class TransactionAdmin(admin.ModelAdmin):
+@admin.register(models.RSI_55_5_MIN)
+class RSI_55_5_MINAdmin(admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','stoploss','rsi','rsi_exit_target','difference','profit')
     list_filter = ("date",)
     list_per_page = 10
-    search_fields = ['symbol']
+    search_fields = ['symbol','date']
