1d68902f7a61f45e8395da1bc211a40b5318f419
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-05 16:43:47 +0530

    XLXS DOWNLODS

--- algo/admin.py
+++ algo/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from . import models
+from import_export.admin import ExportActionMixin
 
 # Register your models here.
 @admin.register(models.RSI_55_5_MIN)
-class RSI_55_5_Min_Admin(admin.ModelAdmin):
+class RSI_55_5_Min_Admin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','stoploss','rsi','rsi_exit_target','difference','profit')
     list_filter = ("date",)
     list_per_page = 10
     search_fields = ['symbol','date']
 
 @admin.register(models.RSI_55_15_MIN)
-class RSI_55_15_Min_Admin(admin.ModelAdmin):
+class RSI_55_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','stoploss','rsi','rsi_exit_target','difference','profit')
     list_filter = ("date",)
     list_per_page = 10
