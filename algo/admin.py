8b051ff063a72364ed0a32a306db7ca86f543379
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-07 01:12:13 +0530

    COMMIT

--- algo/admin.py
+++ algo/admin.py
@@ -5,14 +5,14 @@ from import_export.admin import ExportActionMixin
 # Register your models here.
 @admin.register(models.RSI_55_5_MIN)
 class RSI_55_5_Min_Admin(ExportActionMixin,admin.ModelAdmin):
-    list_display = ('date','symbol','indicate','type','close','stoploss','rsi','rsi_exit_target','difference','profit')
+    list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit')
     list_filter = ("date",)
     list_per_page = 10
     search_fields = ['symbol','date']
 
 @admin.register(models.RSI_55_15_MIN)
 class RSI_55_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
-    list_display = ('date','symbol','indicate','type','close','stoploss','rsi','rsi_exit_target','difference','profit')
+    list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit')
     list_filter = ("date",)
     list_per_page = 10
     search_fields = ['symbol','date']
