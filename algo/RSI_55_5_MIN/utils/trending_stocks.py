8687811fa6ea6eec6dbda7b882c20bd51a200517
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-08 18:27:17 +0530

    COMMIT

--- algo/RSI_55_5_MIN/utils/trending_stocks.py
+++ algo/RSI_55_5_MIN/utils/trending_stocks.py
@@ -5,8 +5,9 @@ def trending(data_frame,intervals):
     for stock in data_frame.columns:
         trend_ema         = talib.EMA(data_frame[stock], timeperiod=intervals[8])
         if data_frame.iloc[-1][stock] > trend_ema[-1]:
-          if data_frame.iloc[-1][stock] > data_frame.iloc[-2][stock]:
-            if trend_ema[-1] > trend_ema[-2]:
-              if trend_ema[-1] > trend_ema[-3]:
-                trend.append(stock)
+          if trend_ema[-1] > trend_ema[-2]:
+            if trend_ema[-2] > trend_ema[-3]:
+              if data_frame.iloc[-1][stock] > data_frame.iloc[-2][stock]:
+                if data_frame.iloc[-1][stock] > data_frame.iloc[-3][stock]:
+                  trend.append(stock)
     return trend
