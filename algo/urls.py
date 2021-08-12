f721748fa17db7d87b128bf386c1c086940e8694
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-13 01:08:07 +0530

    COMMIT

--- algo/urls.py
+++ algo/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 
 router = routers.DefaultRouter()
-router.register(r'RSI_55_5_MIN', views.RSI_55_5_MIN_ViewSet)
+router.register(r'RSI_60_40_5_MIN', views.RSI_60_40_5_MIN_ViewSet)
 router.register(r'RSI_55_15_MIN', views.RSI_55_15_MIN_ViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
 	path('', include(router.urls)),
-    path(r'RSI_55_5_MIN', views.RSI_55_5_MIN, name = 'RSI_55_5_MIN'),
+    path(r'RSI_60_40_5_MIN', views.RSI_60_40_5_MIN, name = 'RSI_60_40_5_MIN'),
     path(r'RSI_55_15_MIN', views.RSI_55_15_MIN, name = 'RSI_55_15_MIN'),
 ]
