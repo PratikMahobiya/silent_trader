fbd1e831e0e09808eeb1330bc512fd6c0b2b78e5
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-17 03:15:01 +0530

    COMMIT

--- algo/admin.py
+++ algo/admin.py
@@ -17,20 +17,6 @@ class CROSS_OVER_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['symbol','date']
 
-@admin.register(models.TD_CA_15_MIN)
-class TD_CA_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
-    list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit','target_percent','trend_rsi','target_hit')
-    list_filter = ("date",)
-    list_per_page = 10
-    search_fields = ['symbol','date']
-
-@admin.register(models.TD_PACA_15_MIN)
-class TD_PACA_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
-    list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit','target_percent','trend_rsi','target_hit')
-    list_filter = ("date",)
-    list_per_page = 10
-    search_fields = ['symbol','date']
-
 @admin.register(models.TH_CA_15_MIN)
 class TH_CA_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit','target_percent','trend_rsi')
@@ -41,6 +27,20 @@ class TH_CA_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
 @admin.register(models.TH_PACA_15_MIN)
 class TH_PACA_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit','target_percent','trend_rsi')
+    list_filter = ("date",)
+    list_per_page = 10
+    search_fields = ['symbol','date']
+
+@admin.register(models.TH_PACA_T2_15_MIN)
+class TH_PACA_T2_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('date','symbol','indicate','type','close','stoploss','target','rsi','emamin','emamax','difference','profit','target_percent','trend_rsi')
+    list_filter = ("date",)
+    list_per_page = 10
+    search_fields = ['symbol','date']
+
+@admin.register(models.BB)
+class BB_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('date','symbol','indicate','type','close','stoploss','lowerband','upperband','rsi','atr','difference','profit')
     list_filter = ("date",)
     list_per_page = 10
     search_fields = ['symbol','date']
