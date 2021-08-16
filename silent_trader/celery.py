722cadad294ef1c12c06068271985d935662ad58
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-16 11:37:43 +0530

    COMMIT

--- silent_trader/celery.py
+++ silent_trader/celery.py
@@ -19,10 +19,10 @@ app.conf.enable_utc = False
 app.conf.update(timezone = 'Asia/Kolkata')
 
 app.conf.beat_schedule = {
-    # 'RSI_60_40_RUNS_IN_EVERY_5_MIN':{
-    #     'task': 'algo.tasks.RSI_60_40_RUNS_5_MIN',
-    #     'schedule': crontab(minute='*/5',hour='9-15', day_of_week='mon-fri'),
-    # },
+    'RSI_60_40_RUNS_IN_EVERY_5_MIN':{
+        'task': 'algo.tasks.RSI_60_40_RUNS_5_MIN',
+        'schedule': crontab(minute='*/5',hour='9-15', day_of_week='mon-fri'),
+    },
     # 'RSI_55_RUNS_IN_EVERY_15_MIN':{
     #     'task': 'algo.tasks.RSI_55_RUNS_15_MIN',
     #     'schedule': crontab(minute='*/15',hour='9-15', day_of_week='mon-fri'),
