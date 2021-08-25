0d5e81739923f31f47d8ffe736e35f889f85cca0
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-08-25 12:32:02 +0530

    BB - update to -1

--- algo/BB_5_MIN/utils/trade.py
+++ algo/BB_5_MIN/utils/trade.py
@@ -14,13 +14,13 @@ def trade_execution(data_frame, intervals, flag, transactions, curr_time):
 # BUYS STOCKS ; ENTRY
 def buys(curr_time,stock, data_frame, rsi, ub, mb, lb, atr, intervals, flag, transactions):
   # If Perv Price is less than LowerBand and Curr Price is Greater Than LowerBand, Curr RSI is less than 40 and Curr RSI is Greater Than Prev RSI, Diff of High and Low is in BtW (ATR and ATR*2)
-  if rsi[-2] < intervals[3] and rsi[-2] > rsi[-3]:
-    if lb[-2] < data_frame['Close'].iloc[-2][stock] and lb[-3] > data_frame['Close'].iloc[-3][stock]:
-      if (atr[-2] <= (data_frame['High'].iloc[-2][stock] - data_frame['Low'].iloc[-2][stock]) <= (atr[-2]*1.4)):
-        flag[stock]['buying_price'] = data_frame['Close'].iloc[-2][stock]
+  if rsi[-1] < intervals[3] and rsi[-1] > rsi[-2]:
+    if lb[-1] < data_frame['Close'].iloc[-1][stock] and lb[-2] > data_frame['Close'].iloc[-2][stock]:
+      if (atr[-1] <= (data_frame['High'].iloc[-1][stock] - data_frame['Low'].iloc[-1][stock]) <= (atr[-1]*1.4)):
+        flag[stock]['buying_price'] = data_frame['Close'].iloc[-1][stock]
         flag[stock]['buy'] = True
         flag['Entry'].append(stock)
-        flag[stock]['stoploss'] = data_frame['Open'].iloc[-2][stock]
+        flag[stock]['stoploss'] = data_frame['Open'].iloc[-1][stock]
         transactions.append({'symbol':stock,'indicate':'Entry','type':'BB','date':curr_time,'close':flag[stock]['buying_price'],'stoploss':flag[stock]['stoploss'],'difference':None,'profit':None})
 
 # SELL STOCK ; EXIT
@@ -30,8 +30,8 @@ def sell(curr_time,stock, data_frame, rsi, ub, mb, lb, atr, intervals, flag, tra
     selling_chart = {0:tr, 1:tr, 2:tr+0.5, 3:tr+1.0, 4:tr+1.5, 5:tr+2.0, 6:tr+2.5, 7:tr+3.0, 8:tr+3.5, 9:tr+4.0}
 
     # If Price is Greater than UpperBand
-    if data_frame['High'].iloc[-2][stock] > ub[-2]:
-        flag[stock]['selling_price'] = data_frame['High'].iloc[-2][stock]
+    if data_frame['High'].iloc[-1][stock] > ub[-1]:
+        flag[stock]['selling_price'] = data_frame['High'].iloc[-1][stock]
         diff          = flag[stock]['selling_price'] - flag[stock]['buying_price']
         profit        = (diff/flag[stock]['buying_price']) * 100
         flag[stock]['buy']      = False
@@ -42,8 +42,8 @@ def sell(curr_time,stock, data_frame, rsi, ub, mb, lb, atr, intervals, flag, tra
         flag[stock]['stoploss'] = 0
 
     # When StopLoss Hits Exits
-    elif data_frame['Close'].iloc[-2][stock] <= flag[stock]['stoploss']:
-        flag[stock]['selling_price'] = data_frame['Close'].iloc[-2][stock]
+    elif data_frame['Close'].iloc[-1][stock] <= flag[stock]['stoploss']:
+        flag[stock]['selling_price'] = data_frame['Close'].iloc[-1][stock]
         diff          = flag[stock]['selling_price'] - flag[stock]['buying_price']
         profit        = (diff/flag[stock]['buying_price']) * 100
         flag[stock]['buy']      = False
@@ -54,14 +54,14 @@ def sell(curr_time,stock, data_frame, rsi, ub, mb, lb, atr, intervals, flag, tra
         flag[stock]['stoploss'] = 0
 
     # for Above 60 RSI Chart SYSTEM
-    elif rsi[-2] >= tr:
-        if rsi[-2] >= flag[stock]['upper_val']:
-            counter         = int(rsi[-2] - tr)
-            flag[stock]['selling_val']     = selling_chart[counter] if 0 <= counter <= 9 else rsi[-2] - 5
-            flag[stock]['upper_val']       = rsi[-2]
-            flag[stock]['stoploss']        = data_frame['Open'].iloc[-2][stock]
-        elif rsi[-2] <= flag[stock]['selling_val'] and data_frame['Close'].iloc[-2][stock] > flag[stock]['buying_price']:
-            flag[stock]['selling_price'] = data_frame['Close'].iloc[-2][stock]
+    elif rsi[-1] >= tr:
+        if rsi[-1] >= flag[stock]['upper_val']:
+            counter         = int(rsi[-1] - tr)
+            flag[stock]['selling_val']     = selling_chart[counter] if 0 <= counter <= 9 else rsi[-1] - 5
+            flag[stock]['upper_val']       = rsi[-1]
+            flag[stock]['stoploss']        = data_frame['Open'].iloc[-1][stock]
+        elif rsi[-1] <= flag[stock]['selling_val'] and data_frame['Close'].iloc[-1][stock] > flag[stock]['buying_price']:
+            flag[stock]['selling_price'] = data_frame['Close'].iloc[-1][stock]
             diff          = flag[stock]['selling_price'] - flag[stock]['buying_price']
             profit        = (diff/flag[stock]['buying_price']) * 100
             flag[stock]['buy']      = False
@@ -76,7 +76,7 @@ def square_off(stock_name,data_frame, intervals, flag, transactions, curr_time):
     # For more than one stock in a list
     if stock_name is None:
         for stock in data_frame['Close'].columns:
-            flag[stock]['selling_price'] = data_frame['Close'].iloc[-2][stock]
+            flag[stock]['selling_price'] = data_frame['Close'].iloc[-1][stock]
             diff          = flag[stock]['selling_price'] - flag[stock]['buying_price']
             profit        = (diff/flag[stock]['buying_price']) * 100
             flag[stock]['buy']      = False
@@ -87,7 +87,7 @@ def square_off(stock_name,data_frame, intervals, flag, transactions, curr_time):
             flag[stock]['stoploss'] = 0
     # for only one stock
     else:
-        flag[stock_name]['selling_price'] = data_frame['Close'].iloc[-2]
+        flag[stock_name]['selling_price'] = data_frame['Close'].iloc[-1]
         diff          = flag[stock_name]['selling_price'] - flag[stock_name]['buying_price']
         profit        = (diff/flag[stock_name]['buying_price']) * 100
         flag[stock_name]['buy']      = False
