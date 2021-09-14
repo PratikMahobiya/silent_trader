adb733a7aaf9f76b9252eb468a8087298e82d381
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-09-15 00:45:52 +0530

    BUG:- REMOVE PROFIT % MULTIPLICATION WITH QUANTITY FROM CRS MAIN MODEL

--- algo/exit_action.py
+++ algo/exit_action.py
@@ -15,7 +15,7 @@ def sell(stock, price, flag, transactions, curr_time, kite_conn_var):
         # -----------------------------------------------
     flag[stock]['selling_price'] = price
     diff          = flag[stock]['selling_price'] - flag[stock]['buying_price']
-    profit        = round((((diff/flag[stock]['buying_price']) * 100) * flag[stock]['quantity']),2)
+    profit        = round((((diff/flag[stock]['buying_price']) * 100)),2)
     diff          = round((diff * flag[stock]['quantity']),2)
     flag[stock]['buy']      = False
     transactions.append({'symbol':stock,'indicate':'Exit','type':'TARGET_HIT','date':curr_time,'close':flag[stock]['selling_price'],'quantity':flag[stock]['quantity'],'stoploss':flag[stock]['stoploss'],'target':flag[stock]['target'],'difference':diff,'profit':profit,'order_id':flag[stock]['order_id'],'order_status':flag[stock]['order_status'],'stoploss_percent':None})
@@ -38,7 +38,7 @@ def sell(stock, price, flag, transactions, curr_time, kite_conn_var):
         # -----------------------------------------------
     flag[stock]['selling_price'] = price
     diff          = flag[stock]['selling_price'] - flag[stock]['buying_price']
-    profit        = round((((diff/flag[stock]['buying_price']) * 100) * flag[stock]['quantity']),2)
+    profit        = round((((diff/flag[stock]['buying_price']) * 100)),2)
     diff          = round((diff * flag[stock]['quantity']),2)
     flag[stock]['buy']      = False
     transactions.append({'symbol':stock,'indicate':'Exit','type':'StopLoss','date':curr_time,'close':flag[stock]['selling_price'],'quantity':flag[stock]['quantity'],'stoploss':flag[stock]['stoploss'],'target':flag[stock]['target'],'difference':diff,'profit':profit,'order_id':None,'order_status':'STOPLOSS_HITTED','stoploss_percent':None})
@@ -62,7 +62,7 @@ def square_off(stock_name, price, flag, transactions, curr_time, kite_conn_var):
       # -----------------------------------------------
       flag[stock_name]['selling_price'] = price
       diff          = flag[stock_name]['selling_price'] - flag[stock_name]['buying_price']
-      profit        = round((((diff/flag[stock_name]['buying_price']) * 100) * flag[stock_name]['quantity']),2)
+      profit        = round((((diff/flag[stock_name]['buying_price']) * 100)),2)
       diff          = round((diff * flag[stock_name]['quantity']),2)
       flag[stock_name]['buy']      = False
       transactions.append({'symbol':stock_name,'indicate':'Square_Off','type':'END_OF_DAY','date':curr_time,'close':flag[stock_name]['selling_price'],'quantity':flag[stock_name]['quantity'],'stoploss':flag[stock_name]['stoploss'],'target':flag[stock_name]['target'],'difference':diff,'profit':profit,'order_id':flag[stock_name]['order_id'],'order_status':flag[stock_name]['order_status'],'stoploss_percent':None})
@@ -80,7 +80,7 @@ def square_off(stock_name, price, flag, transactions, curr_time, kite_conn_var):
       # -----------------------------------------------
       flag[stock_name]['selling_price'] = flag[stock_name]['buying_price']
       diff          = flag[stock_name]['selling_price'] - flag[stock_name]['buying_price']
-      profit        = round((((diff/flag[stock_name]['buying_price']) * 100) * flag[stock_name]['quantity']),2)
+      profit        = round((((diff/flag[stock_name]['buying_price']) * 100)),2)
       diff          = round((diff * flag[stock_name]['quantity']),2)
       flag[stock_name]['buy']      = False
       transactions.append({'symbol':stock_name,'indicate':'Square_Off','type':'CANCELLED','date':curr_time,'close':flag[stock_name]['selling_price'],'quantity':flag[stock_name]['quantity'],'stoploss':flag[stock_name]['stoploss'],'target':flag[stock_name]['target'],'difference':diff,'profit':profit,'order_id':flag[stock_name]['order_id'],'order_status':flag[stock_name]['order_status'],'stoploss_percent':None})
