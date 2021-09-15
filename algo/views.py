e159f88b0be72f973c4444fd4f4fa202f81c8b11
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-09-16 00:54:03 +0530

    BUG SIX

--- algo/views.py
+++ algo/views.py
@@ -3,7 +3,7 @@ from kiteconnect import KiteConnect
 
 # Create your views here.
 def Index(request):
-  api_key = open('./config/api_key.txt','r').read()
+  api_key = open('./algo/config/api_key.txt','r').read()
   try:
     kite = KiteConnect(api_key=api_key)
     kite_url = kite.login_url()
@@ -13,15 +13,15 @@ def Index(request):
   return render(request, 'index.html', context)
 
 def generate_acc_token(request):
-  api_key = open('./config/api_key.txt','r').read()
-  api_secret = open('./config/api_secret.txt','r').read()
+  api_key = open('./algo/config/api_key.txt','r').read()
+  api_secret = open('./algo/config/api_secret.txt','r').read()
   if request.method == 'POST':
     request_token 		= request.POST.get('request_token','')
     try:
       kite = KiteConnect(api_key=api_key)
       data = kite.generate_session(request_token, api_secret=api_secret)
       kite.set_access_token(data["access_token"])
-      with open('./config/access_token.txt','w') as at:
+      with open('./algo/config/access_token.txt','w') as at:
         at.write(data["access_token"])
       ltp = kite.ltp(['NSE:SBIN'])
       context = {'success':'True','access_token': data["access_token"], 'SBI_ltp': ltp['NSE:SBIN']['last_price'],'status':'Now you can "REST IN PEACE".'}
