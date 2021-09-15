7d75486815cdb80bb4c62c83e2a46d6424fe9d24
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-09-16 01:29:43 +0530

    BUG FIX

--- algo/views.py
+++ algo/views.py
@@ -17,16 +17,16 @@ def generate_acc_token(request):
   api_secret = open('./algo/config/api_secret.txt','r').read()
   if request.method == 'POST':
     request_token 		= request.POST.get('request_token','')
-    try:
-      kite = KiteConnect(api_key=api_key)
-      data = kite.generate_session(request_token, api_secret=api_secret)
-      kite.set_access_token(data["access_token"])
-      with open('./algo/config/t_token.txt','w') as at:
-        at.write(data["access_token"])
-      ltp = kite.ltp(['NSE:SBIN'])
-      context = {'access_token': data["access_token"], 'SBI_ltp': ltp['NSE:SBIN']['last_price'],'status':'Now you can "REST IN PEACE".'}
-    except Exception as  e:
-      context = {'status':'Please, Do it once again, My Lord. My Creater. My LUCIFER...'}
+    # try:
+    kite = KiteConnect(api_key=api_key)
+    data = kite.generate_session(request_token, api_secret=api_secret)
+    kite.set_access_token(data["access_token"])
+    with open('./algo/config/t_token.txt','w') as at:
+      at.write(data["access_token"])
+    ltp = kite.ltp(['NSE:SBIN'])
+    context = {'access_token': data["access_token"], 'SBI_ltp': ltp['NSE:SBIN']['last_price'],'status':'Now you can "REST IN PEACE".'}
+    # except Exception as  e:
+    #   context = {'status':'Please, Do it once again, My Lord. My Creater. My LUCIFER...'}
     return render(request, 'success.html', context)
   else:
     context = {'success':False,'status':'Please, Do it once again, My Lord. My Creater. My LUCIFER...'}
