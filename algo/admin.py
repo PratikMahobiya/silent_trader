d3506b3d01ac1dac885189fe2acdd2df89a32fa8
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-09-30 01:13:34 +0530

    UPDATE:- DB

--- algo/admin.py
+++ algo/admin.py
@@ -10,6 +10,22 @@ class ZERODHA_KEYS_Admin(ExportActionMixin,admin.ModelAdmin):
     list_per_page = 10
     readonly_fields = ('access_token','api_key','api_secret')
 
+@admin.register(models.STOCK)
+class STOCK_Admin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('symbol','instrument_key','active')
+
+@admin.register(models.TREND_15M)
+class TREND_15M_Admin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('symbol','rsi')
+
+@admin.register(models.ENTRY_15M)
+class ENTRY_15M_Admin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('symbol')
+
+@admin.register(models.CONFIG_15M)
+class CONFIG_15M_Admin(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('symbol','buy','trend','d_sl_flag','buy_price','sell_price','stoploss','target','f_stoploss','d_stoploss','quantity','count','order_id','order_status')
+
 @admin.register(models.CROSSOVER_15_MIN)
 class CROSSOVER_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','target','stoploss','profit','order_id','stoploss_percent','difference','quantity','order_status')
@@ -20,6 +36,13 @@ class CROSSOVER_15_Min_Admin(ExportActionMixin,admin.ModelAdmin):
 @admin.register(models.CROSSOVER_5_MIN)
 class CROSSOVER_5_MIN_Admin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('date','symbol','indicate','type','close','target','stoploss','profit','order_id','stoploss_percent','difference','quantity','order_status')
+    list_filter = ("date",)
+    list_per_page = 10
+    search_fields = ['symbol','date']
+
+@admin.register(models.CROSSOVER_15_MIN_db)
+class CROSSOVER_15_Min_Admin_db(ExportActionMixin,admin.ModelAdmin):
+    list_display = ('date','symbol','indicate','type','price','target','stoploss','profit','order_id','difference','quantity','order_status')
     list_filter = ("date",)
     list_per_page = 10
     search_fields = ['symbol','date']
--- algo/models.py
+++ algo/models.py
@@ -47,3 +47,60 @@ class CROSSOVER_5_MIN(models.Model):
     	return self.id
     class Meta:
         db_table = 'CROSSOVER_5_MIN'
+
+# ------------------------------------------- CONFIG TABLES -------------------------------------------
+class CROSSOVER_15_MIN_db(models.Model):
+    symbol 					= models.CharField(max_length=100, verbose_name='SYMBOL')
+    indicate    			= models.CharField(max_length=100, verbose_name='INDICATE')
+    type           			= models.CharField(max_length=100, verbose_name='TYPE')
+    date                    = models.DateTimeField(auto_now_add=True)
+    price    				= models.FloatField(verbose_name='PRICE')
+    target   				= models.FloatField(verbose_name='TARGET')
+    stoploss   				= models.FloatField(verbose_name='STOPLOSS')
+    profit 				    = models.FloatField(verbose_name='PROFIT (%)',blank=True,null=True,default=None)
+    order_id                = models.BigIntegerField(verbose_name='ORDER_ID',blank=True,null=True,default=None)
+    order_status            = models.TextField(verbose_name='ORDER_STATUS',max_length=1000)
+    difference 				= models.FloatField(verbose_name='PRICE DIFFERENCE', blank=True, null=True,default=None)
+    quantity                = models.BigIntegerField(verbose_name='QUANTITY')
+    def __int__(self):
+    	return self.id
+    class Meta:
+        db_table = 'CROSSOVER_15_MIN_DB'
+
+class STOCK(models.Model):
+    symbol                  = models.CharField(max_length=100, verbose_name='SYMBOL',unique=True)
+    instrument_key          = models.BigIntegerField(verbose_name='INSTRUMENT KEY')
+    active                  = models.BooleanField(verbose_name='ACTIVE',default=True)
+    def __str__(self):
+        return self.symbol
+    class Meta:
+        db_table = 'STOCK'
+
+class TREND_15M(models.Model):
+    symbol                  = models.CharField(max_length=100, verbose_name='SYMBOL',unique=True)
+    rsi                     = models.FloatField(verbose_name='RSI')
+    class Meta:
+        db_table = 'TREND_15M'
+
+class ENTRY_15M(models.Model):
+    symbol                  = models.CharField(max_length=100, verbose_name='SYMBOL',unique=True)
+    class Meta:
+        db_table = 'ENTRY_15M'
+
+class CONFIG_15M(models.Model):
+    symbol                  = models.CharField(max_length=100, verbose_name='SYMBOL',unique=True)
+    buy                     = models.BooleanField(verbose_name='BUY',default=False)
+    trend                   = models.BooleanField(verbose_name='IN_TREND',default=False)
+    d_sl_flag               = models.BooleanField(verbose_name='D_SL_FLAG',default=False)
+    buy_price               = models.FloatField(verbose_name='BUY PRICE',default=0)
+    sell_price              = models.FloatField(verbose_name='SELL PRICE',default=0)
+    stoploss                = models.FloatField(verbose_name='STOPLOSS',default=0)
+    target                  = models.FloatField(verbose_name='TARGET',default=0)
+    f_stoploss              = models.FloatField(verbose_name='F_SL',default=0)
+    d_stoploss              = models.FloatField(verbose_name='D_SL',default=0)
+    quantity                = models.BigIntegerField(verbose_name='QUANTITY',default=0)
+    count                   = models.BigIntegerField(verbose_name='COUNT',default=0)
+    order_id                = models.BigIntegerField(verbose_name='ORDER ID',default=0)
+    order_status            = models.CharField(max_length=100, verbose_name='ORDER STATUS',default='NONE')
+    class Meta:
+        db_table = 'CONFIG_15M'
