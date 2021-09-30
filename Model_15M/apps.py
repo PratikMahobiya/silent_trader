3a36dc857eb415e503229017e281d3b9188d86bc
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-09-30 20:10:43 +0530

    BUG:- REMOVE NOT USABLE FILES

--- Model_15M/apps.py
+++ Model_15M/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class Model15MConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Model_15M'
-    verbose_name = "........ MODEL 15 Minutes CONFIG TABLES ........"
+    verbose_name = "..... MODEL 15 Minutes CONFIG TABLES ....."
