358e0cdb621dc96333012ddbe757cbd0dc72c096
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-10-17 20:10:49 +0530

    BUG:-

--- silent_trader/settings.py
+++ silent_trader/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'Model_5M.apps.Model5MConfig',
     # ---- NA ----------
     'Model_15_temp.apps.Model15TempConfig',
-    'Model_5_temp.apps.Model5TempConfig',
 
     # Downloaded Apps -------
     'rest_framework',
