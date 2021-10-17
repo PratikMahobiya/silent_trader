15a1b9b85dfa4630e9bfe2d6a9716e3c32d6f748
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-10-17 20:04:19 +0530

    UPDATE:- STOCK LIST UPDATED

--- Model_5_temp/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class Model5TempConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Model_5_temp'
-    verbose_name = "..... MODEL 5 Minutes TEMP CONFIG TABLES ....."
--- algo/tasks.py
+++ algo/tasks.py
@@ -1,12 +1,9 @@
-import os
-import json
 from datetime import datetime, time
 from kiteconnect import KiteConnect
 
 from Model_15M import models
 from Model_5M import models as models_5
 from . import models as models_a
-from . import serializers
 from . import check_ltp
 from . import check_ltp_crs_5
 from celery import shared_task
@@ -18,118 +15,136 @@ from Model_15_temp import models as models_temp
 from . import check_ltp_temp
 from .CROSSOVER_15_MIN_temp.utils import backbone as backbone_CRS_temp
 
-from Model_5_temp import models as models_5_temp
-from . import check_ltp_crs_5_temp
-from .CROSSOVER_5_MIN_temp.utils import backbone as backbone_CRS_5_MIN_temp
-
 @shared_task(bind=True,max_retries=3)
 # initial_setup on DATABASE -------------------------------------
 def get_stocks_configs(self):
   response = {'stock_table': False, 'config_table_15': False, 'config_table_5': False}
   # Stock dict
   stock_dict = {
-    'AUROPHARMA':70401,
-    'AARTIIND':1793,
-    'ADANIENT':6401,
-    'ADANIPORTS':3861249,
-    'AMBUJACEM':325121,
-    'APLLTD':6483969,
-    'APOLLOTYRE':41729,
-    'ASHOKLEY':54273,
-    'AXISBANK':1510401,
-    'BEL' :98049,
-    'BERGEPAINT' :103425,
-    'BHARATFORG' :108033,
-    'BHEL' :112129,
-    'BIOCON' :2911489,
-    'CADILAHC' :2029825,
-    'CHOLAFIN' :175361,
-    'COALINDIA' :5215745,
-    'CONCOR' :1215745,
-    'CUMMINSIND' :486657,
-    'BANKBARODA' :1195009,
-    'CANBK' :2763265,
-    'COLPAL' :3876097,
-    'DLF' :	3771393,
-    'FEDERALBNK' :261889,
-    'ESCORTS' :245249,
-    'GAIL' :1207553,
-    'GODREJCP' :2585345,
-    'GODREJPROP':4576001,
-    'GRANULES':3039233,
-    'HCLTECH' :1850625,
-    'HDFCBANK':341249,
-    'HINDALCO':348929,
-    'HINDPETRO':359937,
-    'ICICIBANK':1270529,
-    'INDUSTOWER':7458561,
-    'IOC':415745,
-    'ITC':424961,
-    'JINDALSTEL':1723649,
-    'JSWSTEEL':3001089,
-    'LT':2939649,
-    'M&M':519937,
-    'M&MFIN':3400961,
-    'MANAPPURAM':4879617,
-    'MCDOWELL-N':2674433,
-    'MFSL':548353,
-    'MOTHERSUMI':1076225,
-    'NAM-INDIA':91393,
-    'NTPC':2977281,
-    'ONGC':633601,
-    'PETRONET':2905857,
-    'PVR':3365633,
-    'SAIL':758529,
-    'SBIN':779521,
-    'SUNPHARMA':857857,
-    'SUNTV':3431425,
-    'TATACHEM':871681,
-    'TATAMOTORS':884737,
-    'TATAPOWER':877057,
-    'TECHM':3465729,
-    'UBL':4278529,
-    'WIPRO':969473,
-    'BANDHANBNK' :579329,
-    'BATAINDIA' :94977,
-    'BHARTIARTL' :2714625,
-    'HDFCLIFE' :119553,
-    'CUB' :1459457,
-    'IDFCFIRSTB' :2863105,
-    'IGL' :2883073,
-    'INDUSINDBK' :1346049,
-    'LUPIN' :2672641,
-    'MARICO' :1041153,
-    'LICHSGFIN' :511233,
-    'NATIONALUM' :1629185,
-    'MUTHOOTFIN' :6054401,
-    'NMDC' :3924993,
-    'RAMCOCEM' :523009,
-    'SBILIFE' :5582849,
-    'TATASTEEL' :895745,
-    'TVSMOTOR' :2170625,
-    'CIPLA' :177665,
-    'HAVELLS' :2513665,
-    'INFY' :408065,
-    'ZEEL' :975873,
-    'PNB' :2730497,
-    'POWERGRID' :3834113,
-    'TORNTPOWER' :3529217,
-    'VEDL' :784129,
-    'GMRINFRA':3463169,
-    'VOLTAS':951809,
-    'BPCL':134657,
-    'DABUR':197633,
-    'IBULHSGFIN':7712001,
-    'ABFRL':7707649,
-    'INDHOTEL':387073,
-    'ADANIGREEN':912129,
-    'ADANITRANS':2615553,
-    'GRASIM':315393,
-    'ICICIGI':5573121,
-    'ICICIPRULI':4774913,
-    'SBICARD':4600577,
-    'TATACONSUM':878593,
-    'UPL':2889473
+    'AARTIIND':	[1793,		'COMMODITY'],
+    'ABFRL':	[7707649,	'nill'],
+    'ACC':		[5633,		'INFRA,COMMODITY'],
+    'ADANIENT':	[6401,		'METAL'],
+    'ADANIGREEN':	[912129,	'ENERGY,COMMODITY'],
+    'ADANIPORTS':	[3861249,	'INFRA'],
+    'AMBUJACEM':	[325121,	'INFRA,MNC,COMMODITY'],
+    'APLLTD':	[6483969,	'nill'],
+    'APOLLOTYRE':	[41729,		'nill'],
+    'ASHOKLEY':	[54273,		'AUTO,INFRA,MNC'],
+    'ASIANPAINT':	[60417,		'CONSUMPTION'],
+    'AUROPHARMA':	[70401,		'PHARMA'],
+    'AXISBANK':	[1510401,	'BANK,FINN'],
+    'BAJAJ-AUTO':	[4267265,	'AUTO,CONSUMPTION'],
+    'BANDHANBNK':	[579329,	'BANK'],
+    'BANKBARODA':	[1195009,	'BANK'],
+    'BANKINDIA':	[1214721,	'BANK'],
+    'BATAINDIA':	[94977,		'MNC'],
+    'BEL':		[98049,		'PSE'],
+    'BERGEPAINT':	[103425,	'CONSUMPTION'],
+    'BHARATFORG':	[108033,	'AUTO'],
+    'BHARTIARTL':	[2714625,	'INFRA,CONSUMPTION'],
+    'BHEL':		[112129,	'PSE'],
+    'BIOCON':	[2911489,	'PHARMA'],
+    'BPCL':		[134657,	'ENERGY,INFRA,PSE,COMMODITY'],
+    'BRITANNIA':	[140033,	'FMCG,MNC,CONSUMPTION'],
+    'CADILAHC':	[2029825,	'PHARMA'],
+    'CANBK':	[2763265,	'BANK'],
+    'CASTROLIND':	[320001,	'MNC'],
+    'CHOLAFIN':	[175361,	'FINN'],
+    'CIPLA':	[177665,	'PHARMA'],
+    'COALINDIA':	[5215745,	'METAL,PSE,COMMODITY'],
+    'COFORGE':	[2955009,	'IT'],
+    'COLPAL':	[3876097,	'FMCG,MNC,CONSUMPTION'],
+    'CONCOR':	[1215745,	'INFRA,PSE'],
+    'CUB':		[1459457,	'nill'],
+    'CUMMINSIND':	[486657,	'MNC'],
+    'DABUR':	[197633,	'FMCG,CONSUMPTION'],
+    'DIVISLAB':	[2800641,	'PHARMA'],
+    'DLF':		[3771393,	'INFRA'],
+    'DMART':	[5097729,	'CONSUMPTION'],
+    'DRREDDY':	[225537,	'PHARMA'],
+    'EMAMILTD':	[3460353,	'FMCG'],
+    'ESCORTS':	[245249,	'nill'],
+    'EXIDEIND':	[173057,	'AUTO,INFRA'],
+    'FEDERALBNK':	[261889,	'BANK'],
+    'GAIL':		[1207553,	'ENERGY,INFRA,PSE'],
+    'GMRINFRA':	[3463169,	'nill'],
+    'GODREJCP':	[2585345,	'FMCG,CONSUMPTION'],
+    'GODREJPROP':	[4576001,	'nill'],
+    'GRANULES':	[3039233,	'nill'],
+    'GRASIM':	[315393,	'INFRA,COMMODITY'],
+    'HAVELLS':	[2513665,	'CONSUMPTION'],
+    'HCLTECH':	[1850625,	'IT'],
+    'HDFC':		[340481,	'FINN'],
+    'HDFCBANK':	[341249,	'BANK,FINN'],
+    'HDFCLIFE':	[119553,	'FINN'],
+    'HINDALCO':	[348929,	'METAL'],
+    'HINDPETRO':	[359937,	'ENERGY,INFRA,PSE,COMMODITY'],
+    'HINDUNILVR':	[356865,	'FMCG,MNC,CONSUMPTION'],
+    'HINDZINC':	[364545,	'METAL,COMMODITY'],
+    'IBULHSGFIN':	[7712001,	'nill'],
+    'ICICIBANK':	[1270529,	'BANK,FINN'],
+    'IDFCFIRSTB':	[2863105,	'BANK'],
+    'IGL':		[2883073,	'INFRA'],
+    'INDIANB':	[3663105,	'BANK'],
+    'INDUSINDBK':	[1346049,	'BANK'],
+    'INDUSTOWER':	[7458561,	'INFRA'],
+    'INFY':		[408065,	'IT'],
+    'IOC':		[415745,	'ENERGY,INFRA,PSE,COMMODITY'],
+    'IRCTC':	[3484417,	'PSE'],
+    'ITC':		[424961,	'FMCG,CONSUMPTION'],
+    'JINDALSTEL':	[1723649,	'METAL,COMMODITY'],
+    'JSWSTEEL':	[3001089,	'METAL,COMMODITY'],
+    'JUBLFOOD':	[4632577,	'FMCG,CONSUMPTION'],
+    'KOTAKBANK':	[492033,	'BANK,FINN'],
+    'LICHSGFIN':	[511233,	'nill'],
+    'LT':		[2939649,	'INFRA'],
+    'LUPIN':	[2672641,	'PHARMA'],
+    'M&M':		[519937,	'AUTO,CONSUMPTION'],
+    'M&MFIN':	[3400961,	'FINN'],
+    'MANAPPURAM':	[4879617,	'nill'],
+    'MARICO':	[1041153,	'FMCG,CONSUMPTION'],
+    'MCDOWELL-N':	[2674433,	'FMCG,MNC,CONSUMPTION'],
+    'MINDTREE':	[3675137,	'IT'],
+    'MUTHOOTFIN':	[6054401,	'FINN'],
+    'NAM-INDIA':	[91393,		'nill'],
+    'NATIONALUM':	[1629185,	'METAL,PSE'],
+    'NETWORK18':	[3612417,	'MEDIA'],
+    'NMDC':		[3924993,	'METAL,PSE,COMMODITY'],
+    'NTPC':		[2977281,	'ENERGY,INFRA,PSE,COMMODITY'],
+    'ONGC':		[633601,	'ENERGY,INFRA,PSE,COMMODITY'],
+    'PETRONET':	[2905857,	'INFRA'],
+    'PFC':		[3660545,	'PSE'],
+    'PNB':		[2730497,	'BANK'],
+    'POWERGRID':	[3834113,	'ENERGY,INFRA,PSE'],
+    'PVR':		[3365633,	'MEDIA'],
+    'RBLBANK':	[4708097,	'BANK'],
+    'RELIANCE':	[738561,	'ENERGY,INFRA,COMMODITY'],
+    'SAIL':		[758529,	'METAL,PSE'],
+    'SBICARD':	[4600577,	'nill'],
+    'SBILIFE':	[5582849,	'FINN'],
+    'SBIN':		[779521,	'BANK,FINN'],
+    'SUNPHARMA':	[857857,	'PHARMA'],
+    'SUNTV':	[3431425,	'MEDIA'],
+    'TATACHEM':	[871681,	'nill'],
+    'TATACONSUM':	[878593,	'FMCG,CONSUMPTION'],
+    'TATAMOTORS':	[884737,	'AUTO'],
+    'TATAPOWER':	[877057,	'ENERGY,INFRA,COMMODITY'],
+    'TATASTEEL':	[895745,	'METAL,COMMODITY'],
+    'TCS':		[2953217,	'IT'],
+    'TECHM':	[3465729,	'IT'],
+    'TITAN':	[897537,	'CONSUMPTION'],
+    'TORNTPOWER':	[3529217,	'COMMODITY'],
+    'TRENT':	[502785,	'CONSUMPTION'],
+    'TVSMOTOR':	[2170625,	'AUTO'],
+    'ULTRACEMCO':	[2952193,	'INFRA,COMMODITY'],
+    'UNIONBANK':	[2752769,	'BANK'],
+    'UPL':		[2889473,	'COMMODITY'],
+    'VEDL':		[784129,	'METAL,MNC,COMMODITY'],
+    'VOLTAS':	[951809,	'CONSUMPTION'],
+    'WELCORP':	[3026177,	'METAL'],
+    'WIPRO':	[969473,	'IT'],
+    'ZEEL':		[975873,	'MEDIA,CONSUMPTION'],
   }
   # Create stocks and config's for trade in stock and config table
   for stock_sym in stock_dict:
@@ -147,10 +162,6 @@ def get_stocks_configs(self):
     # CREATE CONFIG IN FOR 15 MIN TEMP
     if not models_temp.CONFIG_15M_TEMP.objects.filter(symbol = stock_sym).exists():
       models_temp.CONFIG_15M_TEMP(symbol = stock_sym, sector = stock_dict[stock_sym][1]).save()
-
-    # CREATE CONFIG IN FOR 5 MIN TEMP
-    if not models_5_temp.CONFIG_5M_TEMP.objects.filter(symbol = stock_sym).exists():
-      models_5_temp.CONFIG_5M_TEMP(symbol = stock_sym, sector = stock_dict[stock_sym][1]).save()
 
   # Update Responce as per Stock Dict
   if len(models_a.STOCK.objects.all()) == len(stock_dict):
@@ -187,18 +198,16 @@ def Clear_Transactions(self):
   models_a.CROSSOVER_15_MIN.objects.all().delete()
   models_a.CROSSOVER_15_MIN_TEMP.objects.all().delete()
   models_a.CROSSOVER_5_MIN.objects.all().delete()
-  models_a.CROSSOVER_5_MIN_TEMP.objects.all().delete()
   Number_of_trans = []
   Number_of_trans.append(len(models_a.CROSSOVER_15_MIN.objects.all()))
   Number_of_trans.append(len(models_a.CROSSOVER_15_MIN_TEMP.objects.all()))
   Number_of_trans.append(len(models_a.CROSSOVER_5_MIN.objects.all()))
-  Number_of_trans.append(len(models_a.CROSSOVER_5_MIN_TEMP.objects.all()))
   response.update({'N0_of_trans':Number_of_trans,'STATUS':'CLEARED'})
   return response
 
 @shared_task(bind=True,max_retries=3)
 def ltp_of_entries(self):
-  response = {'LTP': False, 'STATUS': 'NONE','ACTIVE_STOCKS': None,'LTP_5': False, 'STATUS_5': 'NONE','ACTIVE_STOCKS_5': None}
+  response = {'LTP': False, 'STATUS': 'NONE','ACTIVE_STOCKS': None,'LTP_30': False, 'STATUS_30': 'NONE','ACTIVE_STOCKS_30': None}
   if datetime.now().time() >= time(9,16,00) and datetime.now().time() < time(15,25,00):
     kite_conn_var = connect_to_kite_connection()
     
@@ -209,10 +218,10 @@ def ltp_of_entries(self):
     except Exception as e:
       pass
 
-    # LTP CRS_5MIN
+    # LTP CRS_30MIN
     try:
       status, active_stocks = check_ltp_crs_5.get_stock_ltp(kite_conn_var)
-      response.update({'LTP_5': True, 'STATUS_5': status,'ACTIVE_STOCKS_5':active_stocks})
+      response.update({'LTP_30': True, 'STATUS_30': status,'ACTIVE_STOCKS_30':active_stocks})
     except Exception as e:
       pass
 
@@ -224,17 +233,10 @@ def ltp_of_entries(self):
     except Exception as e:
       pass
 
-    # LTP CRS_5MIN
-    try:
-      status, active_stocks = check_ltp_crs_5_temp.get_stock_ltp(kite_conn_var)
-      response.update({'LTP_5_TEMP': True, 'STATUS_5_TEMP': status,'ACTIVE_STOCKS_5_TEMP':active_stocks})
-    except Exception as e:
-      pass
-
   elif datetime.now().time() >= time(15,25,00) and datetime.now().time() < time(15,30,00):
-    response.update({'LTP': True, 'STATUS': 'SQUARED OFF','LTP_5_MIN': True, 'STATUS_5_MIN': 'ALL STOCKS ARE SQUARED OFF.'})
+    response.update({'LTP': True, 'STATUS': 'SQUARED OFF','LTP_30_MIN': True, 'STATUS_30_MIN': 'ALL STOCKS ARE SQUARED OFF.'})
   else:
-    response.update({'LTP': True, 'STATUS': 'MARKET IS CLOSED','LTP': True, 'STATUS': 'MARKET IS CLOSED.','LTP_5_MIN': True, 'STATUS_5_MIN': 'MARKET IS CLOSED.'})
+    response.update({'LTP': True, 'STATUS': 'MARKET IS CLOSED','LTP': True, 'STATUS': 'MARKET IS CLOSED.','LTP_30_MIN': True, 'STATUS_30_MIN': 'MARKET IS CLOSED.'})
   return response
 
 @shared_task(bind=True,max_retries=3)
@@ -290,22 +292,4 @@ def CROSS_OVER_RUNS_15_MIN_TEMP(self):
   '''
   status = backbone_CRS_temp.model(intervals, kite_conn_var)
   response.update({'CRS': True, 'STATUS': status, 'ENTRY':list(models_temp.ENTRY_15M_TEMP.objects.all().values_list('symbol',flat=True))})
-  return response
-
-@shared_task(bind=True,max_retries=3)
-def CROSS_OVER_RUNS_5_MIN_TEMP(self):
-  response = {'CRS': False, 'STATUS': 'NONE'}
-
-  # Initialize Kite Connections
-  kite_conn_var       = connect_to_kite_connection()
-  '''
-    -> intervals = [trade_time_period, Num_Of_Days, Upper_rsi, Lower_rsi, EMA_max, EMA_min, trend_time_period, Num_Of_Days, Trend_rsi, Trade_rsi, Num_of_Candles_for_Target]
-  '''
-  intervals      = ['10minute',5,60,55,24,10,'30minute',30,14,14,14,'15minute',5]
-  '''
-  -> Intervals:-
-    ** Make Sure Don't change the Index, Otherwise You Are Responsible for the Disasters.. **
-  '''
-  status = backbone_CRS_5_MIN_temp.model(intervals, kite_conn_var)
-  response.update({'CRS': True, 'STATUS': status, 'ENTRY':list(models_5_temp.ENTRY_5M_TEMP.objects.all().values_list('symbol',flat=True))})
   return response
