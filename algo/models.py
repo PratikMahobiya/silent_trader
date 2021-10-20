83389319173f7ff95ca4b4b79fcbfc021a822f82
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-10-20 20:21:00 +0530

    BUG:-

--- algo/models.py
+++ algo/models.py
@@ -23,7 +23,7 @@ class ZERODHA_KEYS(models.Model):
 
 class PROFIT(models.Model):
     date                    = models.DateField(verbose_name='DATE',null=True, blank=True)
-    model_name              = models.CharField(max_length=100, verbose_name='MODEL NAME',unique=True)
+    model_name              = models.CharField(max_length=100, verbose_name='MODEL NAME')
     top_gain  				= models.FloatField(verbose_name='TOP GAIN',default=0)
     top_gain_time           = models.DateTimeField(verbose_name='TOP GAIN TIME',null=True, blank=True)
     top_loss  				= models.FloatField(verbose_name='TOP LOSS',default=0)
