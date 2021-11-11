940486bc01fa0e89aceaa91bb1491599759c7b48
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-11-11 13:58:04 +0530

    UPDATE:- 30M TREND

--- algo/CROSSOVER_30_MIN/utils/trending_stocks.py
+++ algo/CROSSOVER_30_MIN/utils/trending_stocks.py
@@ -10,16 +10,16 @@ def trending_60(data_frame,intervals):
     rsi         = talib.RSI(data_frame[stock]['Close'].iloc[:-1], timeperiod = intervals[8])
     ema_min     = talib.EMA(data_frame[stock]['Close'].iloc[:-1], timeperiod=40)
     ema_max     = talib.EMA(data_frame[stock]['Close'].iloc[:-1], timeperiod=200)
-    if rsi[-1] >= 40:
-      if ema_min[-1] > ema_max[-1]:
+    if rsi[-1] >= 45:
+      # if ema_min[-1] > ema_max[-1]:
         models.TREND_30M_A(symbol = stock, rsi = rsi[-1]).save()
         conf_obj = models.CONFIG_30M.objects.get(symbol = stock)
         conf_obj.trend = True
         conf_obj.save()
-      else:
-        conf_obj = models.CONFIG_30M.objects.get(symbol = stock)
-        conf_obj.trend = False
-        conf_obj.save()
+      # else:
+      #   conf_obj = models.CONFIG_30M.objects.get(symbol = stock)
+      #   conf_obj.trend = False
+      #   conf_obj.save()
     else:
       conf_obj = models.CONFIG_30M.objects.get(symbol = stock)
       conf_obj.trend = False
@@ -32,16 +32,16 @@ def trending_60_BTST(data_frame,intervals):
     rsi         = talib.RSI(data_frame[stock]['Close'].iloc[:-1], timeperiod = intervals[8])
     ema_min     = talib.EMA(data_frame[stock]['Close'].iloc[:-1], timeperiod=40)
     ema_max     = talib.EMA(data_frame[stock]['Close'].iloc[:-1], timeperiod=200)
-    if rsi[-1] >= 40:
-      if ema_min[-1] > ema_max[-1]:
+    if rsi[-1] >= 45:
+      # if ema_min[-1] > ema_max[-1]:
         models.TREND_30M_A_BTST(symbol = stock, rsi = rsi[-1]).save()
         conf_obj = models.CONFIG_30M_BTST.objects.get(symbol = stock)
         conf_obj.trend = True
         conf_obj.save()
-      else:
-        conf_obj = models.CONFIG_30M_BTST.objects.get(symbol = stock)
-        conf_obj.trend = False
-        conf_obj.save()
+      # else:
+      #   conf_obj = models.CONFIG_30M_BTST.objects.get(symbol = stock)
+      #   conf_obj.trend = False
+      #   conf_obj.save()
     else:
       conf_obj = models.CONFIG_30M_BTST.objects.get(symbol = stock)
       conf_obj.trend = False
