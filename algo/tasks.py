d9454812cd394ee7f06114d17325ebc452f957b2
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-11-12 20:19:19 +0530

    UPDATE:- AUTO DEACTIVATE ZERODHA ENTRY AFTER SquareOff

--- algo/tasks.py
+++ algo/tasks.py
@@ -393,7 +393,7 @@ def ltp_of_entries(self):
       model_config_obj.top_loss_time  = datetime.now().time()
       model_config_obj.top_loss_entry = len(models_temp.ENTRY_15M_TEMP.objects.all().values_list('symbol',flat=True))
     model_config_obj.save()
-    
+
     # # --------------------------------- FREEZE Profit at each LTP ------------------------
     # model_name_list = ['CRS_MAIN', 'CRS_30_MIN', 'CRS_TEMP','CRS_15_MAIN_BTST','CRS_15_TEMP_BTST','CRS_30_MIN_BTST']
     # for index, model_name in enumerate(model_name_list):
@@ -577,6 +577,7 @@ def ltp_of_entries(self):
   elif datetime.now().time() >= time(15,17,00) and datetime.now().time() < time(15,30,00):
     model_name_list = ['CRS_MAIN', 'CRS_TEMP', 'CRS_30_MIN','CRS_15_MAIN_BTST','CRS_15_TEMP_BTST','CRS_30_MIN_BTST']
     for ind, m_name in enumerate(model_name_list):
+      models_a.PROFIT_CONFIG.objects.filter(model_name = m_name).update(zerodha_entry = False)
       model_config_obj = models_a.PROFIT.objects.get(model_name = m_name, date = datetime.now().date())
       if ind == 0:
         profit = models_a.CROSSOVER_15_MIN.objects.filter(indicate = 'Exit',created_on = datetime.now().date()).values_list('profit',flat=True)
