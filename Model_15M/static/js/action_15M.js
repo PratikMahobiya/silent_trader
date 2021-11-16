f06e9b8ef5d718c4151406ac985ad4bf6413a9b3
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-11-16 12:12:54 +0530

    UPDATE:-

--- Model_15M/static/js/action_15M.js
+++ Model_15M/static/js/action_15M.js
@@ -1,3 +1,18 @@
+function getCookie(name) {
+    if (!document.cookie) {
+      return null;
+    }
+  
+    const xsrfCookies = document.cookie.split(';')
+      .map(c => c.trim())
+      .filter(c => c.startsWith(name + '='));
+  
+    if (xsrfCookies.length === 0) {
+      return null;
+    }
+    return decodeURIComponent(xsrfCookies[0].split('=')[1]);
+  }
+
 async function TransactionAPI() {
     let response = await fetch('http://139.59.54.145/crs15m/transactions/')
     let data = await response.json();
