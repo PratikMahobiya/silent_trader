eb10d3bbb482e29e0eab5406829524c5ffa5213d
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-11-17 14:39:34 +0530

    UPDATE:-

--- Model_30M/views.py
+++ Model_30M/views.py
@@ -126,7 +126,7 @@ def Active_Stocks(request):
     active_entry_list = []
     for sym_list in active_entry:
       stock_config_obj = models.CONFIG_30M.objects.get(symbol = sym_list[0])
-      active_entry_list.append({"symbol": sym_list[0], "sector": stock_config_obj.sector,'niftytype':stock_config_obj.niftytype,"price": stock_config_obj.buy_price, "quantity": stock_config_obj.quantity, "date": models_a.CROSSOVER_30_MIN.objects.get(id = sym_list[1]).date + timedelta(hours= 5 , minutes= 30),"placed": stock_config_obj.placed,"reference_id": sym_list[1]})
+      active_entry_list.append({"symbol": sym_list[0] + '/HIT_{}'.format(stock_config_obj.count), "sector": stock_config_obj.sector,'niftytype':stock_config_obj.niftytype,"price": stock_config_obj.buy_price, "quantity": stock_config_obj.quantity, "date": models_a.CROSSOVER_30_MIN.objects.get(id = sym_list[1]).date + timedelta(hours= 5 , minutes= 30),"placed": stock_config_obj.placed,"reference_id": sym_list[1]})
     response.update({'success': True, 'data': active_entry_list})
     return JsonResponse(response)
   return JsonResponse(response)
@@ -249,7 +249,7 @@ def Active_Stocks_BTST(request):
     active_entry_list = []
     for sym_list in active_entry:
       stock_config_obj = models.CONFIG_30M_BTST.objects.get(symbol = sym_list[0])
-      active_entry_list.append({"symbol": sym_list[0], "sector": stock_config_obj.sector,'niftytype':stock_config_obj.niftytype,"price": stock_config_obj.buy_price, "quantity": stock_config_obj.quantity, "date": models_a.CROSSOVER_30_MIN_BTST.objects.get(id = sym_list[1]).date + timedelta(hours= 5 , minutes= 30),"placed": stock_config_obj.placed,"reference_id": sym_list[1]})
+      active_entry_list.append({"symbol": sym_list[0] + '/HIT_{}'.format(stock_config_obj.count), "sector": stock_config_obj.sector,'niftytype':stock_config_obj.niftytype,"price": stock_config_obj.buy_price, "quantity": stock_config_obj.quantity, "date": models_a.CROSSOVER_30_MIN_BTST.objects.get(id = sym_list[1]).date + timedelta(hours= 5 , minutes= 30),"placed": stock_config_obj.placed,"reference_id": sym_list[1]})
     response.update({'success': True, 'data': active_entry_list})
     return JsonResponse(response)
   return JsonResponse(response)
