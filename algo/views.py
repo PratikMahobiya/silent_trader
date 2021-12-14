0594b5b5f7038630ab5ae416937b70c4d1a13909
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-12-14 09:53:31 +0530

    UPDATE:- BUY ON FILTER FREEZE

--- algo/views.py
+++ algo/views.py
@@ -90,10 +90,10 @@ def MODEL_STATUS(request):
 def FREEZE_ALL(request):
   kite_conn_var = connect_to_kite_connection()
   # --------------------------------- FREEZE Profit at each LTP ------------------------
-  crs_main_entry_list = models_15.CONFIG_15M.objects.filter(placed = True).values_list('symbol', flat=True)
-  crs_temp_entry_list = models_temp.CONFIG_15M_TEMP.objects.filter(placed = True).values_list('symbol', flat=True)
-  crs_30_entry_list   = models_30.CONFIG_30M.objects.filter(placed = True).values_list('symbol', flat=True)
-  crs_down_entry_list = models_temp_down.CONFIG_15M_TEMP_DOWN.objects.filter(placed = True).values_list('symbol', flat=True)
+  crs_main_entry_list = models_15.CONFIG_15M.objects.filter(buy = True, placed = True).values_list('symbol', flat=True)
+  crs_temp_entry_list = models_temp.CONFIG_15M_TEMP.objects.filter(buy = True, placed = True).values_list('symbol', flat=True)
+  crs_30_entry_list   = models_30.CONFIG_30M.objects.filter(buy = True, placed = True).values_list('symbol', flat=True)
+  crs_down_entry_list = models_temp_down.CONFIG_15M_TEMP_DOWN.objects.filter(buy = True, placed = True).values_list('symbol', flat=True)
   total_placed_entry = len(crs_main_entry_list) + len(crs_temp_entry_list) + len(crs_30_entry_list) + len(crs_down_entry_list)
 
   model_config_obj               = models.PROFIT.objects.get(model_name = 'OVER_ALL_PLACED', date = datetime.now().date())
