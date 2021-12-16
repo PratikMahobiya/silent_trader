a17709368640b2af1e79c22f07cfe8b517dbd519
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-12-16 18:09:48 +0530

    DYNAMIC TR UPDATE OVERALL MODEL

--- algo/tasks.py
+++ algo/tasks.py
@@ -559,7 +559,7 @@ def ltp_of_entries(self):
     model_profit_config_obj        = models_a.PROFIT_CONFIG.objects.get(model_name = 'OVER_ALL_PLACED')
     if model_config_obj.current_gain > model_profit_config_obj.target:
       model_profit_config_obj.stoploss  = model_profit_config_obj.target - 500
-      model_profit_config_obj.target    = model_profit_config_obj.target + 500
+      model_profit_config_obj.target    = model_profit_config_obj.target + 200
       model_profit_config_obj.count     += 1
       model_profit_config_obj.active    = True
     elif model_profit_config_obj.active is True:
@@ -573,7 +573,7 @@ def ltp_of_entries(self):
         p_l  = p_l_main + p_l_temp + p_l_30 + p_l_down
         models_a.FREEZE_PROFIT(model_name = 'OVER_ALL_PLACED', indicate = 'HIT_{}'.format(model_profit_config_obj.count), price = round(sum(gain), 2), p_l = round(sum(p_l), 2), entry = total_placed_entry, day_hit = 'DAY_HIT_{}'.format(model_profit_config_obj.day_hit),top_price= model_config_obj.top_gain, stoploss = model_config_obj.top_loss).save()
         model_profit_config_obj.day_hit   += 1
-        model_profit_config_obj.target    = 4000
+        model_profit_config_obj.target    = 2000
         model_profit_config_obj.stoploss  = 0
         model_profit_config_obj.count     = 0
         model_profit_config_obj.active    = False
