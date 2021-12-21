ed81bf5e41ff78c1b88836e744e5f91203003301
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-12-21 12:03:10 +0530

    UPDATE:- 15 MIN MAIN TREND 5 MIN VWAP INCLUDED

--- algo/tasks.py
+++ algo/tasks.py
@@ -859,7 +859,7 @@ def CROSS_OVER_RUNS_15_MIN(self):
   '''
     -> intervals = [trade_time_period, Num_Of_Days, Upper_rsi, Lower_rsi, EMA_max, EMA_min, trend_time_period, Num_Of_Days, Trend_rsi, Trade_rsi, Num_of_Candles_for_Target]
   '''
-  intervals      = ['15minute',5,60,55,16,8,'30minute',30,14,14,14]
+  intervals      = ['15minute',5,60,55,16,8,'5minute',30,14,14,14]
   '''
   -> Intervals:-
     ** Make Sure Don't change the Index, Otherwise You Are Responsible for the Disasters.. **
@@ -878,7 +878,7 @@ def CROSS_OVER_RUNS_30_MIN(self):
   '''
     -> intervals = [trade_time_period, Num_Of_Days, Upper_rsi, Lower_rsi, EMA_max, EMA_min, trend_time_period, Num_Of_Days, Trend_rsi, Trade_rsi, Num_of_Candles_for_Target]
   '''
-  intervals      = ['30minute',5,60,55,15,7,'60minute',30,14,14,14]
+  intervals      = ['30minute',5,60,55,15,7,'15minute',30,14,14,14]
   '''
   -> Intervals:-
     ** Make Sure Don't change the Index, Otherwise You Are Responsible for the Disasters.. **
