bb5b5b63317d8982d7282d1db003c8dcfa39f59b
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2021-12-30 01:09:29 +0530

    UPDATE:- TARDE FROM 945

--- algo/CROSSOVER_30_MIN/utils/backbone.py
+++ algo/CROSSOVER_30_MIN/utils/backbone.py
@@ -11,7 +11,7 @@ def model(intervals, kite_conn_var):
     kite_conn_var      = to place orders in zerodha
   '''
   # Regular Trades Execution
-  if datetime.now().time() >= time(9,14,00) and datetime.now().time() < time(15,00,00):
+  if datetime.now().time() >= time(9,43,00) and datetime.now().time() < time(15,00,00):
     # Trend Update i every 15 interval
     trending_stocks_list    = []
     if (15 <= datetime.now().time().minute < 19) or (45 <= datetime.now().time().minute < 49):
