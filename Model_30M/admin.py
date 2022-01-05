4f0051d68b74fa4b28b430ef12d46baefae90c87
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2022-01-06 01:18:22 +0530

    BUG:- FIX

--- Model_30M/admin.py
+++ Model_30M/admin.py
@@ -11,6 +11,6 @@ class ENTRY_30M_Admin(ExportActionMixin,admin.ModelAdmin):
 
 @admin.register(models.CONFIG_30M)
 class CONFIG_30M_Admin(ExportActionMixin,admin.ModelAdmin):
-    list_display = ('symbol','buy''placed','count','buy_price','stoploss','target','sector','niftytype','quantity','return_price','order_id','order_status')
+    list_display = ('symbol','buy','placed','count','buy_price','stoploss','target','sector','niftytype','quantity','return_price','order_id','order_status')
     list_per_page = 10
     search_fields = ['symbol',]
