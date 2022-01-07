9a6fea6e909e75f163095a6583bf1999f7c65cc6
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2022-01-07 12:31:07 +0530

    BUG:- FIX

--- algo/exit_action_crs_30.py
+++ algo/exit_action_crs_30.py
@@ -28,32 +28,32 @@ def sell(stock, price, gain, kite_conn_var):
   models.CONFIG_30M.objects.filter(symbol = stock).update(return_price = gain_val)
   
   # if price hits Fixed StopLoss, Exit
-  if price <= stock_config_obj.stoploss:
-    if stock_config_obj.buy is True:
-      if stock_config_obj.order_id != 0:
-        ord_det = kite_conn_var.order_history(order_id=stock_config_obj.order_id)
-        if ord_det[-1]['status'] == 'COMPLETE':
-          # CALL PLACE ORDER ----
-          order_id, order_status = place_ord(kite_conn_var,stock,stock_config_obj)
-          # ---------------------
-        else:
-          # CALL CANCEL ORDER ----
-          order_id, order_status = cancel_ord(kite_conn_var,stock_config_obj)
-          # ----------------------
+  # if price <= stock_config_obj.stoploss:
+  #   if stock_config_obj.buy is True:
+  #     if stock_config_obj.order_id != 0:
+  #       ord_det = kite_conn_var.order_history(order_id=stock_config_obj.order_id)
+  #       if ord_det[-1]['status'] == 'COMPLETE':
+  #         # CALL PLACE ORDER ----
+  #         order_id, order_status = place_ord(kite_conn_var,stock,stock_config_obj)
+  #         # ---------------------
+  #       else:
+  #         # CALL CANCEL ORDER ----
+  #         order_id, order_status = cancel_ord(kite_conn_var,stock_config_obj)
+  #         # ----------------------
 
-      # diff          = price - stock_config_obj.buy_price
-      # profit        = round((((diff/stock_config_obj.buy_price) * 100)),2)
-      # diff          = round((diff * stock_config_obj.quantity),2) - 100
+  #     diff          = price - stock_config_obj.buy_price
+  #     profit        = round((((diff/stock_config_obj.buy_price) * 100)),2)
+  #     diff          = round((diff * stock_config_obj.quantity),2) - 100
 
-      # trans_data = {'symbol':stock,'sector':stock_config_obj.sector,'niftytype':stock_config_obj.niftytype,'indicate':'Exit','type':'FIXED SL','price':price,'quantity':stock_config_obj.quantity,'stoploss':stock_config_obj.stoploss,'target':stock_config_obj.target,'difference':diff,'profit':profit,'order_id':order_id,'order_status':order_status}
-      # transaction   = serializers.CROSSOVER_30_MIN_Serializer(data=trans_data)
-      # if transaction.is_valid():
-      #   transaction.save()
-      # models.ENTRY_30M.objects.filter(symbol = stock).delete()
-      # stock_config_obj.buy          = False
-      # stock_config_obj.placed       = False
-      # stock_config_obj.count        = 0
-      # stock_config_obj.order_id     = 0
-      # stock_config_obj.save()
+  #     trans_data = {'symbol':stock,'sector':stock_config_obj.sector,'niftytype':stock_config_obj.niftytype,'indicate':'Exit','type':'FIXED SL','price':price,'quantity':stock_config_obj.quantity,'stoploss':stock_config_obj.stoploss,'target':stock_config_obj.target,'difference':diff,'profit':profit,'order_id':order_id,'order_status':order_status}
+  #     transaction   = serializers.CROSSOVER_30_MIN_Serializer(data=trans_data)
+  #     if transaction.is_valid():
+  #       transaction.save()
+  #     models.ENTRY_30M.objects.filter(symbol = stock).delete()
+  #     stock_config_obj.buy          = False
+  #     stock_config_obj.placed       = False
+  #     stock_config_obj.count        = 0
+  #     stock_config_obj.order_id     = 0
+  #     stock_config_obj.save()
 
   return 0
