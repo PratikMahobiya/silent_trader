028e855d74699a2f084c718eeee69451a1043e7d
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2022-01-12 00:56:45 +0530

    UPADTE:- VOL VOLA

--- algo/tasks.py
+++ algo/tasks.py
@@ -46,7 +46,7 @@ def cal_volatility(dt):
 
 def cal_volatility_VOL(dt):
   dt['Return'] = 100 * (dt['Volume'].pct_change())
-  daily_volatility = dt['Return'].std()
+  daily_volatility = dt['Volume'].std()
   return round(daily_volatility,4)
 
 @shared_task(bind=True,max_retries=3)
