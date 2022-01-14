cb7fac0e3062b937c835ee87b1346d866c7ec526
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2022-01-14 10:14:39 +0530

    UPDATE:- REVERSED TO ZERODHA

--- algo/CROSSOVER_15_MIN/utils/get_data.py
+++ algo/CROSSOVER_15_MIN/utils/get_data.py
@@ -1,37 +1,25 @@
-from smartapi import SmartConnect
 from algo import models as models_a
 from datetime import date, datetime, time, timedelta
 from time import sleep
 import pandas as pd
 
-def angelbroking_conn():
-    obj=SmartConnect(api_key="MWxz7OCW",)
-    obj.generateSession("P567723","Qwerty@12")
-    return obj
-
 def download_trade_data(intervals,kite_conn_var):
-  now = datetime.now()
+  if time(9,14,00) <= datetime.now().time() <= time(9,18,00):
+    sleep(60)
+  # else:
+  #   sleep(30)
+  now = date.today()
   from_day = now - timedelta(days=intervals[1])
   df_list = []
   df_key  = []
-  if time(9,14,00) <= datetime.now().time() <= time(9,18,00):
-    sleep(30)
-  angelbroking_conn_val = angelbroking_conn()
   for_trade = models_a.STOCK.objects.filter(active_5 = True, nifty_flag = False).values_list('symbol', flat=True)
   for stock_name in for_trade:
     sleep(0.3)
-    historicParam={
-                  "exchange": "NSE",
-                  "symboltoken": models_a.STOCK.objects.get(symbol = stock_name).token,
-                  "interval": intervals[0],
-                  "fromdate": from_day.strftime("%Y-%m-%d %H:%M"), 
-                  "todate": now.strftime("%Y-%m-%d %H:%M")
-                  }
-    data=pd.DataFrame(angelbroking_conn_val.getCandleData(historicParam)['data'])
-    data_frame = data.set_index(data[0], drop=False, append=False, inplace=False, verify_integrity=False).drop(0, 1)
-    data_frame.rename(columns = {0:'date',1:'Open',2:'High',3:'Low',4:'Close',5:'Volume'}, inplace = True)
-    data_frame.index.names = ['date']
+    data = kite_conn_var.historical_data(instrument_token=models_a.STOCK.objects.get(symbol = stock_name).instrument_key, from_date=from_day, to_date=now, interval=intervals[0])
+    data=pd.DataFrame(data)
+    data_frame = data.set_index(data['date'], drop=False, append=False, inplace=False, verify_integrity=False).drop('date', 1)
+    data_frame.rename(columns = {'open':'Open','high':'High','low':'Low','close':'Close','volume':'Volume'}, inplace = True)
     df_list.append(data_frame)
     df_key.append(stock_name)
-  merged_data_frame = pd.concat(df_list,axis=1,keys=df_key)
+  merged_data_frame = pd.concat(df_list,axis=1,keys=df_key).tz_localize(None)
   return merged_data_frame, for_trade
