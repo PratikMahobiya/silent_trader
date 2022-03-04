fd875b1fbc3fc91e74aed28e421a4a5035754d19
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2022-03-04 18:05:05 +0530

    UPDATE:- SEND REPORT TIME

--- silent_trader/celery.py
+++ silent_trader/celery.py
@@ -21,7 +21,7 @@ app.conf.update(timezone = 'Asia/Kolkata')
 app.conf.beat_schedule = {
     'Send SMS':{
         'task': 'algo.tasks.send_report',
-        'schedule': crontab(minute=30, hour=15, day_of_week='mon-fri'),
+        'schedule': crontab(minute=16, hour=15, day_of_week='mon-fri'),
     },
     'STOCKS_CONFIG_FILES':{
         'task': 'algo.tasks.get_stocks_configs',
